98166211e7f801f3264f471ce888950d8bbcd987
iluvatar1 <woquendo@gmail.com>
2023-04-19 09:59:13 -0500

    Fixed double initialization for an int array

--- 2023-04-19-Arrays2D/row_major.cpp
+++ 2023-04-19-Arrays2D/row_major.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
   const int N = 4;
-  int m[N][N] = {0.0}; //4 by4 matrix with all zeros (default value for ints) allocated on the stack
+  int m[N][N] = {0}; //4 by4 matrix with all zeros (default value for ints) allocated on the stack
   for (unsigned int i = 0; i < N; ++i) {
     for (unsigned int j = 0; j < N; ++j) {
       std::cout << &m[i][j] << "\t";
